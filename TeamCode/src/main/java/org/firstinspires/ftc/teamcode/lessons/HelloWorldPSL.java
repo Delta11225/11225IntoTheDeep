05f0c19ac67586f339e8f86ef9fb71b03ebda935
Delta11225 <lcsdeltarobotics@gmail.com>
2024-08-27 14:45:51 -0400

    Completed Lesson 1 and 2 240827

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lessons/HelloWorldPSL.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lessons/HelloWorldPSL.java
@@ -1,13 +1,14 @@
 package org.firstinspires.ftc.teamcode.lessons;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-@TeleOp
+@Autonomous
 public class HelloWorldPSL extends OpMode {
     @Override
     public void init() {
-        telemetry.addData("Hello","World");
+        telemetry.addData("Hello","Pierson, Lillian, Sophia");
     }
 
     @Override
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lessons/UseStringPSL.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode.lessons;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+@TeleOp()
+
+
+public class UseStringPSL extends OpMode {
+   @Override
+   public void init(){
+      String myName = "Pierson, Sophia, Lillian";
+      int grade = 9;
+      telemetry.addData("Hello", myName);
+      telemetry.addData("Grade", grade);
+   }
+   @Override
+   public void loop(){
+
+   }
+}
+
+
+
+
+
