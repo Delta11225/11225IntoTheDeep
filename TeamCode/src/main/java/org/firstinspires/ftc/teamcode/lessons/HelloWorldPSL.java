17364877390f931e179fa870112168f2d539ecf4
delta11225 <lcsdeltarobotics@gmail.com>
2024-09-10 15:51:36 -0400

    fix roadrunner branch on weyrens computer

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lessons/HelloWorldPSL.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lessons/HelloWorldPSL.java
@@ -1,10 +1,10 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.lessons;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 @TeleOp
-public class HelloWorldJTH extends OpMode {
+public class HelloWorldPSL extends OpMode {
     @Override
     public void init() {
         telemetry.addData("Hello","World");
