31a3b7856d793252c6cc4b7e8dc1e7de199ecd58
Delta11225 <lcsdeltarobotics@gmail.com>
2024-09-17 13:03:45 -0400

    Worked on Challenge #1 Lesson #8 Tonya & Lillian will continue later
    240917

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lessons/ColorDistanceSensorLT.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lessons/ColorDistanceSensorLT.java
@@ -22,9 +22,23 @@ public class ColorDistanceSensorLT extends OpMode {
     //Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
     @Override
     public void loop() {
+        if (sensorDistance.getDistance(DistanceUnit.CM) <= 5) {
+            if ( (sensorColor.red() > sensorColor.green()) && (sensorColor.red() > sensorColor.blue())) {
+                telemetry.addData("Color", "red");
+            } else if ( (sensorColor.blue() > sensorColor.green()) && (sensorColor.blue() > sensorColor.red())) {
+                telemetry.addData("Color", "blue");
+            } else if ((sensorColor.blue() < 2000) || (sensorColor.red() < 2000)) {
+                telemetry.addLine("OBJECT DETECTED-Wrong color");
+            } else {
+                telemetry.addLine("NO OBJECT DETECTED");
+            }
+
+        }
         telemetry.addData("Color vals, r", sensorColor.red());
         telemetry.addData("Color vals, g", sensorColor.green());
         telemetry.addData("Color vals, b", sensorColor.blue());
         telemetry.addData("Distance(cm)", sensorDistance.getDistance(DistanceUnit.CM));
     }
 }
+
+
