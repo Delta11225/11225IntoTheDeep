3778d29c86203a85ed614b4e8089c443c6bc82cc
delta11225 <lcsdeltarobotics@gmail.com>
2024-09-18 12:53:30 -0400

    240927attempted to tune max ang vel line 70.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -54,7 +54,7 @@ public class DriveConstants {
      * motor encoders or have elected not to use them for velocity control, these values should be
      * empirically tuned.
      */
-    public static double kV = 0.33; //tuned test bot on 091324
+    public static double kV = 0.033; //tuned test bot on 091324
     public static double kA = 0.004;
     public static double kStatic = 0;
 
