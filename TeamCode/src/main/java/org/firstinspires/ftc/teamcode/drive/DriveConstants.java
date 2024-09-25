b376c1d35e23aee4ba170dd82efccef1cc4ac6bd
delta11225 <lcsdeltarobotics@gmail.com>
2024-09-25 19:49:45 -0400

    Wrote new code to practice Trajectory Overview

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -67,7 +67,7 @@ public class DriveConstants {
      */
     public static double MAX_VEL = 30;
     public static double MAX_ACCEL = 30;
-    public static double MAX_ANG_VEL = Math.toRadians(60); //tried 30, 60, 120, none worked
+    public static double MAX_ANG_VEL = Math.toRadians(60);
     public static double MAX_ANG_ACCEL = Math.toRadians(60);
 
     /*
