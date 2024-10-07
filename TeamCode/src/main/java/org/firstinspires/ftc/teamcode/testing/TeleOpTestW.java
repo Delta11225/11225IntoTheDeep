8daaf0cf3f2852fb71cd3c5e1d8f975cf26a8659
delta11225 <lcsdeltarobotics@gmail.com>
2024-10-07 16:09:26 -0400

    Worked on Teleop Driver Oriented with new IMU. Fixed errors, need to move motors to Hardware file

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/TeleOpTestW.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/TeleOpTestW.java
@@ -1,69 +1,94 @@
+/* Copyright (c) 2022 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 package org.firstinspires.ftc.teamcode.testing;
 
-
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-import org.firstinspires.ftc.robotcore.external.Func;
-import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AngularVelocity;
-import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
-import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
-import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
-import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
-import org.firstinspires.ftc.robotcore.external.navigation.Position;
-import org.firstinspires.ftc.robotcore.external.navigation.Velocity;
 import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
-import org.firstinspires.ftc.teamcode.utility.Constants;
-import org.firstinspires.ftc.teamcode.utility.ControlConfig;
-import org.firstinspires.ftc.teamcode.utility.HardwareCC;
-import org.firstinspires.ftc.teamcode.utility.HardwareITD;
 
-import java.util.Locale;
-
-import static org.firstinspires.ftc.teamcode.utility.Constants.armCollectPosition;
-import static org.firstinspires.ftc.teamcode.utility.Constants.armHoldPosition;
-import static org.firstinspires.ftc.teamcode.utility.Constants.armScoringPosition;
-import static org.firstinspires.ftc.teamcode.utility.Constants.armTrussHeight;
-import static org.firstinspires.ftc.teamcode.utility.Constants.clampClosedPosition;
-import static org.firstinspires.ftc.teamcode.utility.Constants.clampOpenPosition;
-import static org.firstinspires.ftc.teamcode.utility.Constants.droneHold;
-import static org.firstinspires.ftc.teamcode.utility.Constants.droneLaunch;
-import static org.firstinspires.ftc.teamcode.utility.Constants.linearSlideAutomatedDeployHigh;
-import static org.firstinspires.ftc.teamcode.utility.Constants.linearSlideAutomatedDeployLow;
-import static org.firstinspires.ftc.teamcode.utility.Constants.scissorHookHeightLeft;
-import static org.firstinspires.ftc.teamcode.utility.Constants.scissorHookHeightRight;
-import static org.firstinspires.ftc.teamcode.utility.Constants.scissorLiftHeightLeft;
-import static org.firstinspires.ftc.teamcode.utility.Constants.scissorLiftHeightRight;
-
-
-
-import java.util.Locale;
-
-
+/*
+ * This OpMode shows how to use the new universal IMU interface. This
+ * interface may be used with the BNO055 IMU or the BHI260 IMU. It assumes that an IMU is configured
+ * on the robot with the name "imu".
+ *
+ * The sample will display the current Yaw, Pitch and Roll of the robot.<br>
+ * With the correct orientation parameters selected, pitch/roll/yaw should act as follows:
+ *   Pitch value should INCREASE as the robot is tipped UP at the front. (Rotation about X) <br>
+ *   Roll value should INCREASE as the robot is tipped UP at the left side. (Rotation about Y) <br>
+ *   Yaw value should INCREASE as the robot is rotated Counter Clockwise. (Rotation about Z) <br>
+ *
+ * The yaw can be reset (to zero) by pressing the Y button on the gamepad (Triangle on a PS4 controller)
+ *
+ * This specific sample assumes that the Hub is mounted on one of the three orthogonal planes
+ * (X/Y, X/Z or Y/Z) and that the Hub has only been rotated in a range of 90 degree increments.
+ *
+ * Note: if your Hub is mounted on a surface angled at some non-90 Degree multiple (like 30) look at
+ *       the alternative SensorImuNonOrthogonal sample in this folder.
+ *
+ * This "Orthogonal" requirement means that:
+ *
+ * 1) The Logo printed on the top of the Hub can ONLY be pointing in one of six directions:
+ *    FORWARD, BACKWARD, UP, DOWN, LEFT and RIGHT.
+ *
+ * 2) The USB ports can only be pointing in one of the same six directions:<br>
+ *    FORWARD, BACKWARD, UP, DOWN, LEFT and RIGHT.
+ *
+ * So, To fully define how your Hub is mounted to the robot, you must simply specify:<br>
+ *    logoFacingDirection<br>
+ *    usbFacingDirection
+ *
+ * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list.
+ *
+ * Finally, choose the two correct parameters to define how your Hub is mounted and edit this OpMode
+ * to use those parameters.
+ */
 @TeleOp
-public class TeleOpTest extends OpMode {
 
+public class TeleOpTestW extends LinearOpMode {
+    // The IMU sensor object
+    IMU imu;
 
-    //HardwareITD robot;
 
     public DcMotor rearLeft = null;
     public DcMotor rearRight = null;
     public DcMotor frontLeft = null;
     public DcMotor frontRight = null;
-
-
-
-    // The IMU sensor object
-    IMU imu;
-
-
     private ElapsedTime runtime = new ElapsedTime();
 
     double frontLeftV;
@@ -74,9 +99,9 @@ public class TeleOpTest extends OpMode {
     double forward;
     double right;
     double clockwise;
-    int linearSlideZeroOffset = 0;
+
     double powerMultiplier = 1;
-    double deadZone = Math.abs(0.2);
+
 
     double temp;
     double side;
@@ -84,16 +109,16 @@ public class TeleOpTest extends OpMode {
     double IMUAngle;
     double currentAngle;
 
-    boolean clampIsClosed = false;
-    boolean slideDown = true;
-    boolean slowMode = false;
 
-    boolean armIsScoring = false;
+    //----------------------------------------------------------------------------------------------
+    // Main logic
+    //----------------------------------------------------------------------------------------------
 
     @Override
-    public void init() {
+    public void runOpMode() throws InterruptedException {
+
         // Retrieve and initialize the IMU.
-        // This sample expects the IMU to be in a REV Hub and named "imu".
+
         imu = hardwareMap.get(IMU.class, "imu");
 
         /* Define how the hub is mounted on the robot to get the correct Yaw, Pitch and Roll values.
@@ -118,7 +143,7 @@ public class TeleOpTest extends OpMode {
         // Note: if you choose two conflicting directions, this initialization will cause a code exception.
         imu.initialize(new IMU.Parameters(orientationOnRobot));
 
-        //robot = new HardwareITD(hardwareMap);
+        //initialize drive motors
         rearLeft = hardwareMap.dcMotor.get("rear_left");
         rearLeft.setDirection(DcMotor.Direction.REVERSE);
 
@@ -137,36 +162,29 @@ public class TeleOpTest extends OpMode {
         rearLeft.setPower(0);
         rearRight.setPower(0);
 
+        waitForStart();
 
-
-    }
-
-    @Override
-    public void start() {
         currentAngle = 0;
         runtime.reset();
         imu.resetYaw();
-    }
 
-        @Override
-        public void loop() {
 
-            // Retrieve Rotational Angles and Velocities
+        // Loop and update the dashboard
+        while (!isStopRequested()) {
             YawPitchRollAngles orientation = imu.getRobotYawPitchRollAngles();
-            AngularVelocity angularVelocity = imu.getRobotAngularVelocity(AngleUnit.DEGREES);
 
 
+            telemetry.addData("currentAngle", "%.1f", currentAngle);
             telemetry.addData("Yaw (Z)", "%.2f Deg. (Heading)", orientation.getYaw(AngleUnit.DEGREES));
             telemetry.addData("Pitch (X)", "%.2f Deg.", orientation.getPitch(AngleUnit.DEGREES));
             telemetry.addData("Roll (Y)", "%.2f Deg.\n", orientation.getRoll(AngleUnit.DEGREES));
             telemetry.update();
 
             currentAngle = orientation.getYaw(AngleUnit.DEGREES);
+
             move();
 
-
-
-
+            /*
             if (orientation.getYaw(AngleUnit.DEGREES) < 0.0) {
 
                 telemetry.update();
@@ -176,7 +194,7 @@ public class TeleOpTest extends OpMode {
                 telemetry.addData("currentAngle loop 1", "%.1f", currentAngle);
             }
 
-            if (orientation.getYaw(AngleUnit.DEGREES) >= 0.0) {
+            else  { //if abel is greater than 0 degrees (not negative)
 
                 telemetry.update();
                 move();
@@ -185,14 +203,10 @@ public class TeleOpTest extends OpMode {
                 telemetry.addData("currentAngle loop 2", "%.1f", currentAngle);
             }
 
-            telemetry.addLine("null angle");
-
-
+            telemetry.addLine("out of loop");
+*/
+        }
     }
-
-
-
-
     public void move(){
 
         double theta = Math.toRadians(currentAngle);
@@ -241,6 +255,4 @@ public class TeleOpTest extends OpMode {
 
 
     }
-
-
 }
