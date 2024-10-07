19e39d1e36cf73eda30fbd869a53f68c94c8542b
Delta11225 <lcsdeltarobotics@gmail.com>
2024-10-07 12:58:05 -0400

    Completed RRMockAutoChallenge, updated Y-value for spline and 2nd Math.toRadians from Math.toRadians(0) to Math.toRadians(180)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lessons/RRMockAutoChallenge.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lessons/RRMockAutoChallenge.java
@@ -26,7 +26,7 @@ public class RRMockAutoChallenge extends LinearOpMode {
                 .lineToLinearHeading(new Pose2d(50.5, 48, Math.toRadians(45))) //To basket
                 .lineToLinearHeading(new Pose2d(62, 24, Math.toRadians(0))) //Collects 3rd sample
                 .lineToLinearHeading(new Pose2d(50.5, 48, Math.toRadians(45))) //To basket
-                //.splineToLinearHeading(new Pose2d(25, 10, Math.toRadians(180)),Math.toRadians(0)) //PARK
+                .setReversed(true).splineToLinearHeading(new Pose2d(25, 5, Math.toRadians(180)),Math.toRadians(180)) //PARK
                 .build();
 
 
