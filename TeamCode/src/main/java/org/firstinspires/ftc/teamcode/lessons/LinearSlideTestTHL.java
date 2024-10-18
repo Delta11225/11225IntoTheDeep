c0963e17fc927853fe7fbb443338a25e30c056ca
Delta11225 <lcsdeltarobotics@gmail.com>
2024-10-18 15:01:41 -0400

    Tonya, James, and Lillian plan to run the LinearSlideAutoTestTJL class next time.
    241018

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lessons/LinearSlideTestTHL.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lessons/LinearSlideTestTHL.java
@@ -3,35 +3,33 @@ package org.firstinspires.ftc.teamcode.lessons;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @TeleOp
 public class LinearSlideTestTHL extends OpMode {
 
     private DcMotor linearSlide;
+    double maxHeightLS = 22000;//22104
+    double minHeightLS = 0;
+
 
     @Override
     public void init() {
         linearSlide = hardwareMap.get(DcMotor.class, "linear_slide");
         linearSlide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         linearSlide.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        linearSlide.setDirection(DcMotor.Direction.REVERSE);
 
     }
 
     @Override
     public void loop(){
-        if (gamepad1.dpad_up && linearSlide.getCurrentPosition()!=196){
-            linearSlide.setPower(-0.5);
+        if (gamepad1.dpad_up && linearSlide.getCurrentPosition()<=maxHeightLS){
+            linearSlide.setPower(1);
 
         }
-        if (gamepad1.dpad_up && linearSlide.getCurrentPosition()==196){
-            linearSlide.setPower(0);
-        }
-        if (gamepad1.dpad_down && linearSlide.getCurrentPosition()==-22259) {
-            linearSlide.setPower(0);
-        }
-
-        else if (gamepad1.dpad_down && linearSlide.getCurrentPosition()!=-22259){
-            linearSlide.setPower(0.5);
+        else if (gamepad1.dpad_down && linearSlide.getCurrentPosition()>=minHeightLS) {
+            linearSlide.setPower(-1);
         }
 
         else {
