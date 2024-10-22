c8c466c45d06bdc6eb28cdd6216f473a1ae2069d
Delta11225 <lcsdeltarobotics@gmail.com>
2024-10-22 12:53:52 -0400

    Completed Marker Test challenge using spatial markers, began working on auto for Into The Deep

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lessons/RRMarkerTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lessons/RRMarkerTest.java
@@ -4,32 +4,54 @@ import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
 
 @Autonomous
 public class RRMarkerTest extends LinearOpMode {
+    public Servo leftClaw = null; // leftClaw = port 1
+    public Servo rightClaw = null; // rightClaw = port 0
+    double leftClawClosed = 0.8;
+    double leftClawOpened = 0.4;
+    double rightClawClosed = 0.1;
+    double rightClawOpened = 0.4;
+
     @Override
     public void runOpMode(){
+        leftClaw = hardwareMap.get(Servo.class, "left_claw");
+        rightClaw = hardwareMap.get(Servo.class, "right_claw");
 
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
-        Pose2d startPose = new Pose2d(0, 0, Math.toRadians(90));
+        Pose2d startPose = new Pose2d(0, 60, Math.toRadians(270));
         drive.setPoseEstimate(startPose);
 
         TrajectorySequence traj1 = drive.trajectorySequenceBuilder(startPose)
-                .lineToLinearHeading(new Pose2d(36, 36, Math.toRadians(90)))
+                .lineToLinearHeading(new Pose2d(-65, 20, Math.toRadians(180)))
 
-                .addTemporalMarker(2, () -> {
-                    // This marker runs two seconds into the trajectory
-
-                    // Run your action in here!
+                .addSpatialMarker(new Vector2d(-35, 20), () -> {
+                    leftClaw.setPosition(leftClawOpened);
+                    rightClaw.setPosition(rightClawOpened);
                 })
+                .back(0.0001)
+                .addSpatialMarker(new Vector2d(-65,20), () -> {
+                    leftClaw.setPosition(leftClawClosed);
+                    rightClaw.setPosition(rightClawClosed);
+                    })
+                .waitSeconds(0.5)
+                .back(5)
+                .lineToLinearHeading(new Pose2d(-60, 60, Math.toRadians(90)))
+
 
                 .build();//center spike mark
 
+        //Init Servos
+        leftClaw.setPosition(leftClawClosed);
+        rightClaw.setPosition(rightClawClosed);
 
         waitForStart();
+
         drive.followTrajectorySequence(traj1);
 
 
